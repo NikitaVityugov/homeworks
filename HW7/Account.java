b0de4be8797b4b88dfb999f69407f6e93478053d
NikitaVityugov <damon25@bk.ru>
2022-08-24 12:08:12 +0300

    добавил поле idNumber, сделал поиск клиента по счету, но надо менять и номер клиента для поиска, не могу сделать без этого.

--- HW7/Account.java
+++ HW7/Account.java
@@ -1,13 +1,18 @@
 class Client extends Bank {
     String name;
     int age;
-    public Client(String name, int age) {
+    int idNumber;
+    public Client(String name, int age, int idNumber) {
         this.name = name;
         this.age = age;
-
+        this.idNumber = idNumber;
     }
     @Override
     public String toString() {
+        return name;
+    }
+    public String findClient(int idNumber) {
+        System.out.println(name);
         return name;
     }
 }
@@ -15,9 +20,11 @@ class Client extends Bank {
 public class Account extends Bank {
     int accountNumber;
     double goldBalance;
-    public Account(int accountNumber, double goldBalance) {
+    int idNumber;
+    public Account(int accountNumber, double goldBalance, int idNumber) {
         this.accountNumber = accountNumber;
         this.goldBalance = goldBalance;
+        this.idNumber = idNumber;
     }
 
     @Override
@@ -25,13 +32,3 @@ public class Account extends Bank {
         return String.valueOf(accountNumber);
     }
 }
-     /*    List<Account> listOfAccount = new ArrayList<>();
-        static Account clientAccount1 = new Account(12345678, 5.4);
-        static Account clientAccount2 = new Account(22345678, 5.0);
-        static Account clientAccount3 = new Account(32345678, 5.5);
-        static Account clientAccount4 = new Account(42345678, 5.3);
-        static Account clientAccount5 = new Account(52345678, 6.3);
-        static Account clientAccount6 = new Account(62345678, 6.1);
-        static Account clientAccount7 = new Account(72345678, 5.0);*/
-     /*   List<Account> listOfAccount = Arrays.asList(clientAccount1, clientAccount2, clientAccount3, clientAccount4,
-                clientAccount5, clientAccount6, clientAccount7);*/
--- HW7/Bank.java
+++ HW7/Bank.java
@@ -2,30 +2,40 @@ import java.util.*;
 
 public class Bank {
     public static void main(String[] args) {
+        int idNumber;
         Bank bank = new Bank();
 
-        Client client1 = new Client("Nikita", 29);
-        Client client2 = new Client("Alexander", 55);
-        Client client3 = new Client("Irina", 54);
-        Client client4 = new Client("Anastasia", 30);
+        Client client1 = new Client("Nikita", 29, 10);
+        Client client2 = new Client("Alexander", 55, 11);
+        Client client3 = new Client("Irina", 54, 12);
+        Client client4 = new Client("Anastasia", 30, 13);
         bank.listOfClient.addAll(Arrays.asList(client1, client2, client3, client4));
-        bank.hash.put(client1, List.of(new Account(1234, 5.4)));
-        bank.hash.put(client2, Arrays.asList(new Account(1235, 5.0),
-                new Account(1236, 5.3)));
-        bank.hash.put(client3, Arrays.asList(new Account(1237, 5.1),
-                new Account(1238, 5.2)));
-        bank.hash.put(client4, Arrays.asList(new Account(1239, 5.2),
-                new Account(1240, 5.5)));
 
-        List<Account> hash1 = bank.hash.get(client2);
-        System.out.println(hash1);
+        Account clientAccount1 = new Account(1234, 5.4, 10);
+        Account clientAccount2 = new Account(1235, 5.0, 11);
+        Account clientAccount3 = new Account(1236, 5.3, 11);
+        Account clientAccount4 = new Account(1237, 5.1, 12);
+        Account clientAccount5 = new Account(1238, 5.2, 12);
+        Account clientAccount6 = new Account(1239, 5.2, 13);
+        Account clientAccount7 = new Account(1240, 5.5, 13);
+        bank.listOfAccount.addAll(Arrays.asList(clientAccount1, clientAccount2, clientAccount3, clientAccount4,
+                clientAccount5, clientAccount6, clientAccount7));
 
-/*
-        for (HashMap.Entry<Client, List<Account>> hash2: bank.hash.entrySet()) {
-            System.out.println("Имя клиента: " + hash2.getKey() + ", который имеет счета:  " + hash2.getValue());
-        }
-*/
+        bank.hash.put(client1, List.of(clientAccount1));
+        bank.hash.put(client2, List.of(clientAccount2, clientAccount3));
+        bank.hash.put(client3, List.of(clientAccount4, clientAccount5));
+        bank.hash.put(client4, List.of(clientAccount6, clientAccount7));
+
+        bank.getAccounts(client2);
+        client1.findClient(11);
     }
     HashMap<Client, List<Account>> hash = new HashMap<>();
     List<Client> listOfClient = new ArrayList<>();
+    List<Account> listOfAccount = new ArrayList<>();
+
+    Client getAccounts(Client client) {
+        List<Account> hash1 = hash.get(client);
+        System.out.println(hash1);
+        return client;
+    }
 }
